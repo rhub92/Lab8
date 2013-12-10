45f0e0368c1f9aab077a82e1cf6585649b570f3e
Ryan Hub <c15ryan.hub@usafa.edu>
2013-12-10 11:11:59 -0700

    Got the robot reading the wall, at this point its just messing with the algorithm

--- main.c
+++ main.c
@@ -11,13 +11,29 @@ int main(void) {
     initMotorTimer();
     initializeSensorSubsystem();
 
-    while(1) {
-  	  initializeLeftSensor();
-  		if (voltageReading() > 0x299)
-  			moveRobotLeft();
-  		else
-  			stopRobot();
-    }
+	//while (centerSensorReading() || rightSensorReading() < 0x2ff) {
+		//moveRobotForward();
+		while (1) {
+			unsigned int centersensor = centerSensorReading();
+			unsigned int rightsensor = rightSensorReading();
 
+
+
+			if (rightsensor > 0x250 && centersensor < 0x250)
+				moveRobotForward();
+			else if(rightsensor > 0x230 && centersensor > 0x310) {
+				moveRobotSharpLeft();
+				_delay_cycles(750000);
+			}
+			//else if(rightsensor < 0x250 && centersensor < 0x250)
+				//moveRobotSharpRight();
+			else
+				moveRobotForward();
+
+
+		//}
+
+		}
 	return 0;
 }
+
